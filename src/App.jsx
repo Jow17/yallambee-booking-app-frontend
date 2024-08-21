0a3b4c3531faa9a90fba9c873e9121aef1df65bd
Jow17 <jonotheowl@outlook.com.au>
2024-08-21 15:40:56 +1000

    Removed Links from App.jsx, created basic Header in Header.jsx and added Header.module.css in styling folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-import React from "react";
-import AdminDashboard from "./pages/AdminDashboard";
-import BookingPage from "./pages/BookingPage";
-import HomePage from "./pages/HomePage";
-import PropertyListingPage from "./pages/PropertyListingPage";
-import ProfilePage from "./pages/ProfilePage";
-import { Link, Routes, Route } from 'react-router-dom';
-
-
+import React from "react"
+import AdminDashboard from "./pages/AdminDashboard"
+import BookingPage from "./pages/BookingPage"
+import HomePage from "./pages/HomePage"
+import PropertyListingPage from "./pages/PropertyListingPage"
+import ProfilePage from "./pages/ProfilePage"
+import Header from "./components/Header"
+import { Link, Routes, Route, Router } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/property-listing">Properties</Link></li>
-          <li><Link to="/booking">Bookings</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><Link to="/admin">Admin Dashboard</Link></li>
-        </ul>
-      </nav>
-
+    <> 
+      <Header/>
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/property-listing" element={<PropertyListingPage />} />
@@ -29,10 +19,9 @@ function App() {
         <Route exact path="/profile" element={<ProfilePage />} />
         <Route exact path="/admin" element={<AdminDashboard />} />
       </Routes>
-    
     </>
      
-  );
+  )
 }
 
-export default App;
+export default App
