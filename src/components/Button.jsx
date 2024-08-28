ffc4c54a1d90a0f0d649302e85e4911ea8ad7ffc
shayzimm <shay.zimmerle@gmail.com>
2024-08-28 09:58:18 +0800

    added button component and 404 page

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Button = ({ children, onClick, varient = "primary" }) => {
+  const primary =
+    "text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800";
+
+  const danger =
+    "text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5  dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800";
+  return (
+    <button
+      onClick={onClick}
+      type="button"
+      className={
+        varient === "primary" ? primary : varient === "danger" && danger
+      }
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
