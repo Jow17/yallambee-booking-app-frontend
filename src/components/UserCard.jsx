d40a01a73d467f25cb6b75c774ce788b1417fe8e
shayzimm <shay.zimmerle@gmail.com>
2024-08-31 23:37:09 +0800

    update usercard styling

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -14,22 +14,38 @@ const UserCard = ({ user, onDelete, onEdit }) => {
     <div className="bg-slate-50 shadow-md rounded-lg overflow-hidden">
       <div className="p-4">
         <div>
-          <span className="font-bold">First name: </span>
+          <span className="font-bold">Username: </span>
+          <span>{user.username}</span>
+        </div>
+        <div>
+          <span className="font-bold">First Name: </span>
           <span>{user.firstName}</span>
         </div>
         <div>
-          <span className="font-bold">Last name: </span>
+          <span className="font-bold">Last Name: </span>
           <span>{user.lastName}</span>
         </div>
         <div>
           <span className="font-bold">Email: </span>
           <span>{user.email}</span>
         </div>
+        <div>
+          <span className="font-bold">Phone: </span>
+          <span>{user.phone}</span>
+        </div>
+        <div>
+          <span className="font-bold">Date of Birth: </span>
+          <span>{new Date(user.dob).toLocaleDateString()}</span>
+        </div>
+        <div>
+          <span className="font-bold">Admin: </span>
+          <span>{user.isAdmin ? "Yes" : "No"}</span>
+        </div>
       </div>
 
       <div className="flex gap-2 justify-between items-center p-4 bg-gray-100">
         <Button onClick={onEditUser}>Edit</Button>
-        <Button varient="danger" onClick={() => onDelete(user._id)}>
+        <Button variant="danger" onClick={() => onDelete(user._id)}>
           Delete
         </Button>
       </div>
